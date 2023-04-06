463f1650d4f2bb7fb75634d68aca5c4467ddc0f1
slivcaigor <slivcaigor@gmail.com>
2023-04-06 15:46:05 +0200

    Second Function

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -2,10 +2,6 @@
  In questo esercizio vi chiedo di definire qualche funzione di utilità che poi potete usare per poter fare operazioni complesse nei vostri programma principale.
  Scrivete nel vostro programma principale Program.cs le seguenti funzioni di base:
 
-    void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]“. 
-
-    Potete prendere quella fatta in classe questa mattina
-
     int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
 
     int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
@@ -34,10 +30,12 @@ namespace csharp_functions
         {
             int[] numeri = { 1, 2, 3, 4, 5, 6, 7, 8 };
             StampaArray(numeri);
+
+            Console.WriteLine(Quadrato(8));
+
         }
 
-
-       static void StampaArray(int[] array)
+        static void StampaArray(int[] array)
         {
             Console.Write("[");
             for (int i = 0; i < array.Length; i++)
@@ -51,6 +49,14 @@ namespace csharp_functions
             Console.Write("]");
             Console.ReadLine();
         }
+
+        static int Quadrato(int numero)
+        {
+            int quadrato = numero * numero;
+            return quadrato;
+        }
+
+
 
     }
 }
