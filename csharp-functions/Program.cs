58be3def62c2e297382f62af4dda648fbfd4cb76
slivcaigor <slivcaigor@gmail.com>
2023-04-06 16:53:50 +0200

    Comments

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -17,16 +17,21 @@ namespace csharp_functions
         {
             Console.Write("Inserisci il numero di elementi che vuoi aggiungere: ");
             int numeroElementi;
+            // converto l'input utente in numero intero con "int.TryParse" e assegno il valore convertito alla variabile "numeroElementi" usando l'operatore "out"
+            // se l'utente inserisce una stringa vuota o non valida, "int.TryParse" restituisce "false" e quindi viene valutata la seconda condizione in cui il valore di "numeroElementi" deve essere minore o uguale a 0 per essere vera
             while (!int.TryParse(Console.ReadLine(), out numeroElementi) || numeroElementi <= 0)
             {
                 Console.Write("Inserisci un numero intero valido maggiore di zero: ");
             }
 
+            // creo nuovo array con lunghezza pari al valore inserito dall'utente
             int[] numeri = new int[numeroElementi];
 
+            // ciclo che prende l'input dell'utente per ogni elemento dell'array numeri
             for (int i = 0; i < numeroElementi; i++)
             {
                 Console.Write($"Inserisci il numero per l'elemento {i + 1}: ");
+                // utilizzo "int.TryParse" per convertire in numero intero il valore inserito dall'utente e lo assegno all'elemento corrispondente dell'array "numeri"
                 while (!int.TryParse(Console.ReadLine(), out numeri[i]))
                 {
                     Console.Write($"Inserisci un numero intero valido per l'elemento {i + 1}: ");
@@ -36,6 +41,7 @@ namespace csharp_functions
             int[] quadrati = ElevaArrayAlQuadrato(numeri);
 
             Console.WriteLine($"Array iniziale: [{string.Join(", ", numeri)}]");
+            // uso "string.Join" per unire gli elementi dell'array separando con una virgola e uno spazio
             Console.WriteLine($"Array con numeri al quadrato: [{string.Join(", ", quadrati)}]");
             Console.WriteLine($"Array iniziale non modificato: [{string.Join(", ", numeri)}]");
 
@@ -61,12 +67,17 @@ namespace csharp_functions
             //Console.WriteLine(SommaElementiArray(quadrati));
         }
 
+        // prende come parametro un array di numeri interi
         static void StampaArray(int[] array)
         {
             Console.Write("[");
+
+            // itero attraverso ogni elemento dell'array
             for (int i = 0; i < array.Length; i++)
             {
+                // per ogni elemento dell'array stampo il suo valore
                 Console.Write(array[i]);
+                // verifico se e' l'ultimo elemento dell'array per stampare una virgola e spazio dopo ogni elemento
                 if (i < array.Length - 1)
                 {
                     Console.Write(", ");
@@ -75,29 +86,43 @@ namespace csharp_functions
             Console.Write("]");    
         }
 
+        //  prende come parametro un numero intero e restituisce il suo quadrato 
         static int Quadrato(int numero)
         {
+            // dichiaro una variabile "quadrato" di tipo intero e assegno il valore del parametro "numero" elevato al quadrato
             int quadrato = numero * numero;
+            //restituisco il valore della variabile "quadrato" come risultato della funzione
             return quadrato;
         }
 
+        // prende come parametro un array di numeri interi e restituisce un nuovo array contenente il quadrato di ogni elemento dell'array originale
         static int[] ElevaArrayAlQuadrato(int[] array)
         {
+            // dichiaro un nuovo array "quadrati" che ha la dimensione uguale alla lunghezza dell'array originale
             int[] quadrati = new int[array.Length];
+
+            // itero attraverso ogni elemento dell'array originale
             for (int i = 0; i < array.Length; i++)
             {
+                // per ogni elemento calcolo il suo quadrato e lo assegno all'elemento corrispondente del array nuovo "quadrati"
                 quadrati[i] = array[i] * array[i];
             }
+            // restituisco l'array con i quadrati degli elementi
             return quadrati;
         }
 
+        // prende come paramentro un array di interi e restituisce la somma di tutti gli elementi dell'array
         static int SommaElementiArray(int[] array)
         {
+            // dichiaro e inizializzo la variabile "somma" a 0
             int somma = 0;
+            // itero attraverso ogni elemento dell'array 
             for (int i = 0; i < array.Length; i++)
             {
+                // per ogni elemento, il suo valore viene sommato al valore della variabile "somma"
                 somma += array[i];
             }
+            // faccio il return per restituire il valore della variabile somma
             return somma;
         }
 
